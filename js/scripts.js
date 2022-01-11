e00f6ab801eb79d4b59996b838453eaa7260d2b3
Amaryah Lockett <amaryahg@gmail.com>
2022-01-11 09:58:05 -0500

    initial message

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function newSrc() {
+  document.getElementById("MyFrame").src="https://www.poetryfoundation.org/poets/langston-hughes";
+    document.getElementById("MyFrame").Title="Langston Hughes Biography"
+}
